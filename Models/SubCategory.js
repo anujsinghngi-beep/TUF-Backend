c02a26507a519e14cbb900db639e372515ce7410
Anuj <singhanuj2507@gmail.com>
2025-10-03 15:50:22 +0530

    Enhacement- Added husky in TUF Backend

---
+++ Models/SubCategory.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../Config/Database");
+
+const Subcategory = sequelize.define("SubCategory", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.ENUM("easy", "medium", "hard"),
+    allowNull: false,
+  },
+  description: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+});
